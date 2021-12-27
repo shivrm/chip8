17b6b343383ef18f62c2d444ca71eb3af6fb70db
ShivramS2006 <shivrams.2006@gmail.com>
2021-12-27 18:59:16 +0530

    💩 Create a script to parse instructions

---
+++ src/instr_parse.py
@@ -0,0 +1,29 @@
+import toml
+import re
+
+with open("./src/instr.toml", "r") as f:
+    data = toml.load(f)
+
+regexed = {}
+for instr, hex in data.items():
+    regexed_hex = (
+        hex.replace("nnn", "([0-9a-f]{3})")
+        .replace("x", "([0-9a-f])")
+        .replace("y", "([0-9a-f])")
+        .replace("n", "([0-9a-f])")
+    )
+    regexed[instr] = regexed_hex
+    
+instruction = '2abc'
+
+for instr, hex in regexed.items():
+    match = re.match(hex, instruction)
+    
+    if not match:
+        continue
+    
+    print(instr, match.groups())
+    break
+    
+else:
+    print("No matches")
