56ab1fc91d71963c202d8d66b604d4d250dd4d50
ShivramS2006 <shivrams.2006@gmail.com>
2021-12-27 14:28:40 +0530

    🚧 Add `VF` `ST` and `DT` registers, and `stack`

--- src/cpu.py
+++ src/cpu.py
@@ -2,13 +2,19 @@ class Registers:
     V = bytearray(16)
     I = 0x00
 
+    VF = 0x00
+
     PC = 0x00
     SP = 0x00
+    
+    ST = 0x00
+    DT = 0x00
 
 class CPU(object):
     def __init__(self) -> None:
         
         self.memory = bytearray(16 ** 3)
+        self.stack = bytearray(16)
         self.registers = Registers()
         
     def load_sprites(self):
