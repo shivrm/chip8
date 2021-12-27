2c948b4914933f7153a2c8f5f29c6ed3cb23bdc6
ShivramS2006 <shivrams.2006@gmail.com>
2021-12-27 14:25:28 +0530

    ✨ Add `load_sprites` to load sprites
    Sprites are stored in `src/sprites`

--- src/cpu.py
+++ src/cpu.py
@@ -10,3 +10,10 @@ class CPU(object):
         
         self.memory = bytearray(16 ** 3)
         self.registers = Registers()
+        
+    def load_sprites(self):
+        with open('./src/sprites', 'rb') as f:
+            binary = list(f.read(80))
+            
+        bin_strs = [bin(b)[2:-4] for b in binary]
+        self.sprites = [bin_strs[idx: idx + 5] for idx in range(0, 80, 5)]
