d1b9e77043006499ca3466ca1310f43739070e68
ShivramS2006 <shivrams.2006@gmail.com>
2021-12-27 14:45:48 +0530

    🎨 Format `src/cpu.py`

--- src/cpu.py
+++ src/cpu.py
@@ -6,20 +6,21 @@ class Registers:
 
     PC = 0x00
     SP = 0x00
-    
+
     ST = 0x00
     DT = 0x00
 
+
 class CPU(object):
     def __init__(self) -> None:
-        
+
         self.memory = bytearray(16 ** 3)
         self.stack = bytearray(16)
         self.registers = Registers()
-        
+
     def load_sprites(self):
-        with open('./src/sprites', 'rb') as f:
+        with open("./src/sprites", "rb") as f:
             binary = list(f.read(80))
-            
+
         bin_strs = [bin(b)[2:-4] for b in binary]
-        self.sprites = [bin_strs[idx: idx + 5] for idx in range(0, 80, 5)]
+        self.sprites = [bin_strs[idx : idx + 5] for idx in range(0, 80, 5)]
