4ca4803b8d21e83b3022e642b8599ee5c12a4a43
ShivramS2006 <shivrams.2006@gmail.com>
2021-12-28 11:19:53 +0530

    ♻️ Move `display` code into `Display` class

--- src/display.py
+++ src/display.py
@@ -3,23 +3,25 @@ import pygame
 BLACK = pygame.Color(0, 0, 0)
 WHITE = pygame.Color(255, 255, 255)
 
-pygame.init()
-DISPLAY = pygame.display.set_mode((640, 320))
+class Display:
+    def __init__(self) -> None:
+        pygame.init()
+        self.display = pygame.display.set_mode((640, 320))
 
-def update():
-    pygame.display.update()
-    for evt in pygame.event.get():
-        if evt.type == pygame.QUIT:
-            pygame.quit()
+    def update(self):
+        pygame.display.update()
+        for evt in pygame.event.get():
+            if evt.type == pygame.QUIT:
+                pygame.quit()
 
-def set(x, y):
-    pygame.draw.rect(DISPLAY, WHITE, pygame.Rect(x * 10, y * 10, 10, 10))
-    pygame.display.update()
-    
-def unset(x, y):
-    pygame.draw.rect(DISPLAY, BLACK, pygame.Rect(x * 10, y * 10, 10, 10))
-    pygame.display.update()
+    def set(self, x, y):
+        pygame.draw.rect(self.display, WHITE, pygame.Rect(x * 10, y * 10, 10, 10))
+        pygame.display.update()
+        
+    def unset(self, x, y):
+        pygame.draw.rect(self.display, BLACK, pygame.Rect(x * 10, y * 10, 10, 10))
+        pygame.display.update()
 
-def quit():
-    pygame.quit()
+    def quit(self):
+        pygame.quit()
     
