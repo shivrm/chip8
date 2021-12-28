2415f7d4b96297fad9833dd48243b947de8008d4
ShivramS2006 <shivrams.2006@gmail.com>
2021-12-28 12:33:15 +0530

    📝 Add comments to `display.py`

--- src/display.py
+++ src/display.py
@@ -1,28 +1,35 @@
 import pygame
 
+# Define constants
 BLACK = pygame.Color(0, 0, 0)
 WHITE = pygame.Color(255, 255, 255)
 
 class Display:
     def __init__(self) -> None:
+        # Initialize display
         pygame.init()
         self.display = pygame.display.set_mode((640, 320))
 
     def update(self):
+        # Update display and handle events
         pygame.display.update()
         for evt in pygame.event.get():
             if evt.type == pygame.QUIT:
                 pygame.quit()
 
     def set(self, x, y):
+        # Set a pixel on the screen
         pygame.draw.rect(self.display, WHITE, pygame.Rect(x * 10, y * 10, 10, 10))
         pygame.display.update()
         
     def unset(self, x, y):
+        # Unset a pixel on the screen
         pygame.draw.rect(self.display, BLACK, pygame.Rect(x * 10, y * 10, 10, 10))
         pygame.display.update()
 
     def flip(self, x, y):
+        # Flip a pixel on the screen.
+        # Return true if the pixel was unset
         color = self.display.get_at(x * 10, y * 10)
         
         if color == WHITE:
@@ -34,5 +41,6 @@ class Display:
             return False
 
     def quit(self):
+        # Close the display
         pygame.quit()
     
