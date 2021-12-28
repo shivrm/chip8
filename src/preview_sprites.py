ca3062ea601fd65d3b38ec1a2e73f5c7bb0768fa
Shivram <86292088+shivrm@users.noreply.github.com>
2021-12-28 13:01:51 +0530

    Add `display` property to `CPU` (#4)
    * Move parse files to `src/utils`
    * Make `Display` object oriented (#3)
     * ✨ Add a `display` using pygame
     * ♻️ Move `display` code into `Display` class
     * Add flip method to `Display`
     * 📝 Add comments to `display.py`
    * Add `clear` method to display
    * ♻️ Fix implementation of display in `CPU`

---
+++ src/preview_sprites.py
@@ -0,0 +1,21 @@
+# Open file and read the bytes
+with open('./src/sprites', 'rb') as f:
+    binary = list(f.read(80))
+
+# Convert the list of bytes to a list of binary strings containing
+# only the first 4 bits of the 8-bit binary representation
+bin_strs = [bin(b)[2:-4] for b in binary]
+
+# Then split the list into groups of 5, containing each sprite
+sprites = [bin_strs[idx: idx + 5] for idx in range(0, 80, 5)]
+
+# Pad the binary representation, then replace the 0s and 1s with
+# spaces and blocks respectively. After this, print the sprite
+for sprite in sprites:
+    for line in sprite:
+        padded = line.zfill(4)
+        formatted = padded.replace('0', '  ').replace('1', '██')
+        
+        print(formatted)
+        
+    print()
