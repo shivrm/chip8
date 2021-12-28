83b71603f45f0539734abf55564a54e22f7ad08c
Shivram <86292088+shivrm@users.noreply.github.com>
2021-12-28 12:40:23 +0530

    Make `Display` object oriented (#3)
    * ✨ Add a `display` using pygame
    * ♻️ Move `display` code into `Display` class
    * Add flip method to `Display`
    * 📝 Add comments to `display.py`

--- src/display.py
+++ src/display.py
@@ -1,25 +1,45 @@
 import pygame
 
+# Define constants
 BLACK = pygame.Color(0, 0, 0)
 WHITE = pygame.Color(255, 255, 255)
 
-pygame.init()
-DISPLAY = pygame.display.set_mode((640, 320))
+class Display:
+    def __init__(self) -> None:
+        # Initialize display
+        pygame.init()
+        self.display = pygame.display.set_mode((640, 320))
 
-def update():
-    pygame.display.update()
-    for evt in pygame.event.get():
-        if evt.type == pygame.QUIT:
-            pygame.quit()
+    def update(self):
+        # Update display and handle events
+        pygame.display.update()
+        for evt in pygame.event.get():
+            if evt.type == pygame.QUIT:
+                pygame.quit()
 
-def set(x, y):
-    pygame.draw.rect(DISPLAY, WHITE, pygame.Rect(x * 10, y * 10, 10, 10))
-    pygame.display.update()
-    
-def unset(x, y):
-    pygame.draw.rect(DISPLAY, BLACK, pygame.Rect(x * 10, y * 10, 10, 10))
-    pygame.display.update()
+    def set(self, x, y):
+        # Set a pixel on the screen
+        pygame.draw.rect(self.display, WHITE, pygame.Rect(x * 10, y * 10, 10, 10))
+        pygame.display.update()
+        
+    def unset(self, x, y):
+        # Unset a pixel on the screen
+        pygame.draw.rect(self.display, BLACK, pygame.Rect(x * 10, y * 10, 10, 10))
+        pygame.display.update()
 
-def quit():
-    pygame.quit()
-    
+    def flip(self, x, y):
+        # Flip a pixel on the screen.
+        # Return true if the pixel was unset
+        color = self.display.get_at(x * 10, y * 10)
+        
+        if color == WHITE:
+            pygame.draw.rect(self.display, BLACK, pygame.Rect(x * 10, y * 10, 10, 10))
+            return True
+        
+        else:
+            pygame.draw.rect(self.display, WHITE, pygame.Rect(x * 10, y * 10, 10, 10))
+            return False
+
+    def quit(self):
+        # Close the display
+        pygame.quit()
