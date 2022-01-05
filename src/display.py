dbe369203da24b1d76bb031353e91ff51a94d677
Shivram <86292088+shivrm@users.noreply.github.com>
2022-01-05 13:31:18 +0530

    Extend Display API by adding list of pressed keys and display exit property (#5)
    * Add display property that stores pressed keys
    * Add exit attribute to check if display has stopped

--- src/display.py
+++ src/display.py
@@ -4,17 +4,35 @@ import pygame
 BLACK = pygame.Color(0, 0, 0)
 WHITE = pygame.Color(255, 255, 255)
 
+keyboard = "1234QWERASDFZXCV"
+keycodes = [pygame.key.key_code(key) for key in keyboard]
+
 class Display:
     def __init__(self) -> None:
         # Initialize display
         pygame.init()
         self.display = pygame.display.set_mode((640, 320))
+        
+        self.exit = False
+        self.pressed_keys = [False] * 16
 
     def update(self):
         # Update display and handle events
-        pygame.display.update()
-        for evt in pygame.event.get():
-            if evt.type == pygame.QUIT:
+        pygame.display.flip()
+        
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key in keycodes:
+                    idx = keycodes.index(event.key)
+                    self.pressed_keys[idx] = True
+                                        
+            if event.type == pygame.KEYUP:
+                if event.key in keycodes:
+                    idx = keycodes.index(event.key)
+                    self.pressed_keys[idx] = False
+            
+            if event.type == pygame.QUIT:
+                self.exit = True
                 pygame.quit()
 
     def set(self, x, y):
@@ -39,10 +57,11 @@ class Display:
         else:
             pygame.draw.rect(self.display, WHITE, pygame.Rect(x * 10, y * 10, 10, 10))
             return False
-
+    
     def clear(self):
         self.display.fill(BLACK)
-
+          
     def quit(self):
         # Close the display
+        self.exit = True
         pygame.quit()
