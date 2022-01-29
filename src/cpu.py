f35ce419303c0c030bb58f2354dace88386072ff
shivrm <shivrams.2006@gmail.com>
2022-01-29 10:27:12 +0530

    Implement `handle` and `load_rom` in cpu

--- src/cpu.py
+++ src/cpu.py
@@ -1,5 +1,6 @@
 from display import Display
-
+import instr
+from time import sleep
 
 class Registers:
     V = bytearray(16)
@@ -7,7 +8,7 @@ class Registers:
 
     VF = 0x00
 
-    PC = 0x00
+    PC = 0x200
     SP = 0x00
 
     ST = 0x00
@@ -15,16 +16,17 @@ class Registers:
 
 
 class CPU(object):
-    def __init__(self) -> None:
-
+    def __init__(self, rom) -> None:
         # Initialize the memory, stack and registers
         self.memory = bytearray(16 ** 3)
         self.stack = bytearray(16)
         self.registers = Registers()
 
+        self.load_rom(rom)
         self.display = Display()
 
         self.load_sprites()
+        self.loop()
 
     def load_sprites(self):
         # Open the file containing the sprites and read the bytes
@@ -39,11 +41,29 @@ class CPU(object):
         self.display.update()  # Handle display events
 
         # Get the instruction from memory
-        instr = self.memory[self.registers.PC : self.registers.PC + 1]
+        instr = self.memory[self.registers.PC : self.registers.PC + 2]
+
+        if instr.hex() == "0000":
+            self.display.quit()
+            return
+
         self.handle(instr)  # Handle the instruction
 
-        self.registers.PC += 1  # Increment the program counter
+        sleep(0.01)
+        self.registers.PC += 2  # Increment the program counter
+        
+        self.loop()
 
-    def handle(instr):
-        # NotImplemented
-        pass
+    def handle(self, opcode):
+        opname, args = instr.parse(opcode)
+
+        instr.call(self, opname, args)
+        
+    def load_rom(self, rom_loc):
+        with open(rom_loc, "rb") as f:
+            data = f.read()
+            
+        for idx, byte in enumerate(data):
+            self.memory[0x200 + idx] = byte
+            
+CPU("test/vftest.ch8")
