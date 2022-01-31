a62a7528c2fdc61b3991908bf626f901f31bffc2
shivrm <shivrams.2006@gmail.com>
2022-01-31 10:42:24 +0530

    Format and add comments to `cpu` and `display`

--- src/cpu.py
+++ src/cpu.py
@@ -2,19 +2,28 @@ from .display import Display
 from . import instr
 from time import sleep
 
+
 class Registers:
     V = bytearray(16)
     I = 0x00
 
-    PC = 0x200
-    SP = 0x00
+    PC = 0x200 # Program counter
+    SP = 0x00 # Stack pointer
 
+    # Sound and display timers
     ST = 0x00
     DT = 0x00
 
 
 class CPU(object):
-    def __init__(self, file, fgcolor=(255, 255, 255), bgcolor=(0, 0, 0), keys="X123QWEASDZC4RFV", speed=60) -> None:
+    def __init__(
+        self,
+        file,
+        fgcolor=(255, 255, 255),
+        bgcolor=(0, 0, 0),
+        keys="X123QWEASDZC4RFV",
+        speed=60,
+    ) -> None:
         # Initialize the memory, stack and registers
         self.memory = bytearray(16 ** 3)
         self.stack = [0] * 16
@@ -23,7 +32,7 @@ class CPU(object):
         self.sleep_time = 1 / speed
 
         self.load_rom(file)
-        self.display = Display(fgcolor, bgcolor)
+        self.display = Display(fgcolor, bgcolor, keys)
 
         self.load_sprites()
         self.loop()
@@ -39,34 +48,55 @@ class CPU(object):
 
     def loop(self):
         while True:
-            self.display.update()  # Handle display events
+            
+            # Try to update display - raises error if display has been quit
+            try:
+                self.display.update()  # Handle display events
+            except:
+                return
 
             # Get the instruction from memory
             instr = self.memory[self.registers.PC : self.registers.PC + 2]
 
+            # Stop execution if instruction is 0x00 0x00
             if instr.hex() == "0000":
                 input("Program end")
                 self.display.quit()
                 return
 
+            # Decrement the delay timer if it is nonzero
             if self.registers.DT:
                 self.registers.DT -= 1
 
-            self.handle(instr)  # Handle the instruction
+            self.handle(instr) # Handle the instruction
 
             sleep(self.sleep_time)
             self.registers.PC += 2  # Increment the program counter
 
     def handle(self, opcode):
+        """Handles the instruction
+
+        Args:
+            opcode (bytearray[2]): A bytearray of length 2, containing
+            the bytes for the instruction opcode
+        """
+        
+        # Parse the instruction into operation name and arguments
         opname, args = instr.parse(opcode)
 
+        # For debug
         # v_data = " ".join(["{:02x}".format(x) for x in self.registers.V])
         # print(f"{hex(self.registers.PC)} | {opcode.hex()} ({opname}); V: {v_data}, I: {self.registers.I}")
-        instr.call(self, opname, args)
         
+        # Call the function associated with the instruction
+        instr.call(self, opname, args)
+
     def load_rom(self, rom_loc):
+        
+        # Open ROM and read data
         with open(rom_loc, "rb") as f:
             data = f.read()
-            
+
+        # Write to memory starting at 0x200
         for idx, byte in enumerate(data):
             self.memory[0x200 + idx] = byte
--- src/display.py
+++ src/display.py
@@ -1,61 +1,80 @@
 import os
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "True"
+
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "True"
 
 import pygame
 
+
 class Display:
-    def __init__(self, fgcolor=(255, 255, 255), bgcolor=(0, 0, 0),
-                 keys="X123QWEASDZC4RFV") -> None:
-        
+    def __init__(
+        self, fgcolor=(255, 255, 255), bgcolor=(0, 0, 0), keys="X123QWEASDZC4RFV"
+    ) -> None:
+
         # Initialize display
         pygame.init()
         self.display = pygame.display.set_mode((640, 320))
-        
+
+        # Set color and keyboard
         self.set_color(fgcolor, bgcolor, clear=True)
         self.set_keys(keys)
-        
-        self.exit = False
-        self.pressed_keys = [False] * 16
+
+        self.exit = False # Whether the display has been closed
+        self.pressed_keys = [False] * 16 # Which keys have been pressed
 
     def update(self):
-        # Update display and handle events
+        
+        if self.exit:
+            return
+        
+        # Update display
         pygame.display.flip()
 
+        # Handle events
         for event in pygame.event.get():
+            
+            # If any key is pressed, mark it as pressed
             if event.type == pygame.KEYDOWN:
                 if event.key in self.keycodes:
                     idx = self.keycodes.index(event.key)
                     self.pressed_keys[idx] = True
 
+            # If unpressed, make the corresponding value False
             if event.type == pygame.KEYUP:
                 if event.key in self.keycodes:
                     idx = self.keycodes.index(event.key)
                     self.pressed_keys[idx] = False
 
+            # Check if close button was pressed
             if event.type == pygame.QUIT:
                 self.exit = True
                 pygame.quit()
 
     def flip(self, x, y):
-        if self.exit: return
-        
+        if self.exit:
+            return
+
         # Flip a pixel on the screen.
         # Return true if the pixel was unset
         color = self.display.get_at((x * 10, y * 10))
 
         if color == self.FGCOLOR:
-            pygame.draw.rect(self.display, self.BGCOLOR, pygame.Rect(x * 10, y * 10, 10, 10))
+            pygame.draw.rect(
+                self.display, self.BGCOLOR, pygame.Rect(x * 10, y * 10, 10, 10)
+            )
             return True
 
         else:
-            pygame.draw.rect(self.display, self.FGCOLOR, pygame.Rect(x * 10, y * 10, 10, 10))
+            pygame.draw.rect(
+                self.display, self.FGCOLOR, pygame.Rect(x * 10, y * 10, 10, 10)
+            )
             return False
 
     def set_color(self, fgcolor=(255, 255, 255), bgcolor=(0, 0, 0), clear=False):
         self.FGCOLOR = pygame.Color(*fgcolor)
         self.BGCOLOR = pygame.Color(*bgcolor)
-        
-        if clear: self.clear()
+
+        if clear:
+            self.clear()
 
     def set_keys(self, keys="X123QWEASDZC4RFV"):
         self.keyboard = keys
